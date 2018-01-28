d61a43dd6ecfdb279d4cf24b3bc127c36f063c49
Leandro Almeida <32276456+iLeandro@users.noreply.github.com>
2018-01-28 20:36:38 +0000

    Add files via upload

---
+++ Euromilhões/Euromilhões/Euromilhoes.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Euromilhões
+{
+    partial class Euromilhoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Euromilhoes));
+            this.btnBet = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lblStar1 = new System.Windows.Forms.Label();
+            this.lblStar2 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtNumber3 = new System.Windows.Forms.TextBox();
+            this.txtNumber4 = new System.Windows.Forms.TextBox();
+            this.txtNumber5 = new System.Windows.Forms.TextBox();
+            this.txtStar1 = new System.Windows.Forms.TextBox();
+            this.txtStar2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBet
+            // 
+            this.btnBet.Font = new System.Drawing.Font("Proxima Nova Lt", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBet.Location = new System.Drawing.Point(346, 267);
+            this.btnBet.Name = "btnBet";
+            this.btnBet.Size = new System.Drawing.Size(131, 57);
+            this.btnBet.TabIndex = 0;
+            this.btnBet.Text = "Bet";
+            this.btnBet.UseVisualStyleBackColor = true;
+            this.btnBet.Click += new System.EventHandler(this.btnBet_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Proxima Nova Lt", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(61, 68);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 78);
+            this.lbl1.TabIndex = 1;
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Proxima Nova Lt", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(207, 68);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(0, 78);
+            this.lbl2.TabIndex = 2;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Proxima Nova Lt", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(357, 68);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(0, 78);
+            this.lbl3.TabIndex = 3;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Proxima Nova Lt", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(505, 68);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(0, 78);
+            this.lbl4.TabIndex = 4;
+            // 
+            // lbl5
+            // 
+            this.lbl5.AutoSize = true;
+            this.lbl5.Font = new System.Drawing.Font("Proxima Nova Lt", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl5.Location = new System.Drawing.Point(648, 68);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(0, 78);
+            this.lbl5.TabIndex = 5;
+            // 
+            // lblStar1
+            // 
+            this.lblStar1.AutoSize = true;
+            this.lblStar1.Font = new System.Drawing.Font("Proxima Nova Lt", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStar1.Location = new System.Drawing.Point(307, 175);
+            this.lblStar1.Name = "lblStar1";
+            this.lblStar1.Size = new System.Drawing.Size(0, 78);
+            this.lblStar1.TabIndex = 6;
+            // 
+            // lblStar2
+            // 
+            this.lblStar2.AutoSize = true;
+            this.lblStar2.Font = new System.Drawing.Font("Proxima Nova Lt", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStar2.Location = new System.Drawing.Point(451, 175);
+            this.lblStar2.Name = "lblStar2";
+            this.lblStar2.Size = new System.Drawing.Size(0, 78);
+            this.lblStar2.TabIndex = 7;
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Font = new System.Drawing.Font("Proxima Nova Lt", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber1.Location = new System.Drawing.Point(76, 8);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(63, 66);
+            this.txtNumber1.TabIndex = 8;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Font = new System.Drawing.Font("Proxima Nova Lt", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber2.Location = new System.Drawing.Point(227, 8);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(63, 66);
+            this.txtNumber2.TabIndex = 9;
+            // 
+            // txtNumber3
+            // 
+            this.txtNumber3.Font = new System.Drawing.Font("Proxima Nova Lt", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber3.Location = new System.Drawing.Point(370, 12);
+            this.txtNumber3.Name = "txtNumber3";
+            this.txtNumber3.Size = new System.Drawing.Size(63, 66);
+            this.txtNumber3.TabIndex = 10;
+            // 
+            // txtNumber4
+            // 
+            this.txtNumber4.Font = new System.Drawing.Font("Proxima Nova Lt", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber4.Location = new System.Drawing.Point(523, 12);
+            this.txtNumber4.Name = "txtNumber4";
+            this.txtNumber4.Size = new System.Drawing.Size(63, 66);
+            this.txtNumber4.TabIndex = 11;
+            // 
+            // txtNumber5
+            // 
+            this.txtNumber5.Font = new System.Drawing.Font("Proxima Nova Lt", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumber5.Location = new System.Drawing.Point(663, 12);
+            this.txtNumber5.Name = "txtNumber5";
+            this.txtNumber5.Size = new System.Drawing.Size(63, 66);
+            this.txtNumber5.TabIndex = 12;
+            // 
+            // txtStar1
+            // 
+            this.txtStar1.Font = new System.Drawing.Font("Proxima Nova Lt", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStar1.Location = new System.Drawing.Point(199, 175);
+            this.txtStar1.Name = "txtStar1";
+            this.txtStar1.Size = new System.Drawing.Size(63, 66);
+            this.txtStar1.TabIndex = 13;
+            // 
+            // txtStar2
+            // 
+            this.txtStar2.Font = new System.Drawing.Font("Proxima Nova Lt", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStar2.Location = new System.Drawing.Point(555, 175);
+            this.txtStar2.Name = "txtStar2";
+            this.txtStar2.Size = new System.Drawing.Size(63, 66);
+            this.txtStar2.TabIndex = 14;
+            // 
+            // Euromilhoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 351);
+            this.Controls.Add(this.txtStar2);
+            this.Controls.Add(this.txtStar1);
+            this.Controls.Add(this.txtNumber5);
+            this.Controls.Add(this.txtNumber4);
+            this.Controls.Add(this.txtNumber3);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.lblStar2);
+            this.Controls.Add(this.lblStar1);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnBet);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Euromilhoes";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBet;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lblStar1;
+        private System.Windows.Forms.Label lblStar2;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtNumber3;
+        private System.Windows.Forms.TextBox txtNumber4;
+        private System.Windows.Forms.TextBox txtNumber5;
+        private System.Windows.Forms.TextBox txtStar1;
+        private System.Windows.Forms.TextBox txtStar2;
+    }
+}
+
---
+++ Euromilhões/Euromilhões/Euromilhoes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Euromilhões
+{
+    public partial class Euromilhoes : Form
+    {
+        public Euromilhoes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBet_Click(object sender, EventArgs e)
+        {
+            //RANDOM
+            Random randomNumber = new Random();
+            Random randomStar = new Random();
+
+
+            //NUMBERS
+            int randomNumber1 = randomNumber.Next(1, 50);
+            int randomNumber2 = randomNumber.Next(1, 50);
+            int randomNumber3 = randomNumber.Next(1, 50);
+            int randomNumber4 = randomNumber.Next(1, 50);
+            int randomNumber5 = randomNumber.Next(1, 50);
+
+
+            //STARS
+            int randomStar1 = randomStar.Next(1, 10);
+            int randomStar2 = randomStar.Next(1, 10);
+
+
+            //STORE VALUES
+            lbl1.Text = randomNumber1.ToString();
+            lbl2.Text = randomNumber2.ToString();
+            lbl3.Text = randomNumber3.ToString();
+            lbl4.Text = randomNumber4.ToString();
+            lbl5.Text = randomNumber5.ToString();
+
+            lblStar1.Text = randomStar1.ToString();
+            lblStar2.Text = randomStar2.ToString();
+
+
+            if (txtNumber1.Text == randomNumber1.ToString() || txtNumber2.Text == randomNumber2.ToString() || txtNumber3.Text == randomNumber3.ToString() || txtNumber4.Text == randomNumber4.ToString() || txtNumber5.Text == randomNumber5.ToString())
+            {
+                MessageBox.Show("Congrats!!");
+            }
+
+            if (txtStar1.Text == randomStar1.ToString() || txtStar2.Text == randomStar2.ToString())
+            {
+                MessageBox.Show("Congrats!!");
+            }
+        }
+    }
+}
